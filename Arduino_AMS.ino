f24f8aa38ce085d86f69090db1a27cb547240c70
Kiehyun Kevin Park <guitar79@gs.hs.kr>
2021-05-24 10:00:41 +0900

    update

--- Arduino_AMS.ino
+++ Arduino_AMS.ino
@@ -1,12 +1,15 @@
 //https://forum.arduino.cc/t/how-to-set-an-alarm-with-the-ds1302-real-time-clock/594882
 //https://m.blog.naver.com/chandong83/221451714944
 
+#include <EEPROM.h>
+String versionSrt = "arduino AMS by Kevin";
+
+
 #include <ThreeWire.h>  
 #include <RtcDS1302.h>
 
 //ThreeWire myWire(4,5,2); // IO, SCLK, CE
 ThreeWire myWire(3,2,4); // IO, SCLK, RST
-
 //RTC 라이브러리 생성
 RtcDS1302<ThreeWire> Rtc(myWire);
 
@@ -19,23 +22,18 @@ http://www.devicemart.co.kr/
 // 미세 먼지 없을 때 초기 V 값 0.35
 // 공기청정기 위 등에서 먼지를 가라앉힌 후 voltage값 개별적으로 측정 필요
 #define no_dust 0.35
-
 // 아두이노 - 미세 먼지 센서 연결
 int dustout=A0;
 int v_led=13;
-
 // 센서로 읽은 값 변수 선언
 float vo_value=0;
-
 // 센서로 읽은 값을 전압으로 측정 변수
 float sensor_voltage=0;
-
 // 실제 미세 먼지 밀도 변수
 float dust_density=0;
 
 
 // for the temperature and hubmidity sensor
-
  #include <DHT.h>
  #define DHT22_PIN 2
  #define DHTTYPE DHT22
@@ -46,8 +44,15 @@ float dust_density=0;
 
   
 void setup () 
-{
+  {
     Serial.begin(9600);
+
+    //EEPROM
+    writeStringToEEPROM(0, versionSrt);
+    String retrievedString = readStringFromEEPROM(0);
+    Serial.print("The String we read from EEPROM: ");
+    Serial.println(retrievedString);
+    
     pinMode(v_led,OUTPUT); // 적외선 led 출력으로 설정
     
     //컴파일 시점의 날짜(__DATE__)와 시간(__TIME__)을 시리얼모니터에 표시
@@ -58,16 +63,15 @@ void setup ()
 
     //RTC 모듈 라이브러리 시작
     Rtc.Begin();
-    
     //RTC 시간을 설정 컴파일된 날짜로
     setTRTCDateTime();
 
     //U8G displayer
     u8gStart();
-}
+  }
 
 void loop () 
-{
+  {
     //RTC 모듈의 현재 시간 얻기
     RtcDateTime now = Rtc.GetDateTime();
 
@@ -98,4 +102,4 @@ void loop ()
     u8gDraw();
     //1초 대기...후 다시 loop 시작
     delay(1000); // ten seconds
-}
+  }
